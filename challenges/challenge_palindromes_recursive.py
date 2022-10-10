b73689ba75cbbe6103f29f3290304fcf3289b900
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-10 16:01:29 -0300

    requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    low = low_index
+    high = high_index
+
+    if len(word) == 0:
+        return False
+
+    if high == 0:
+        return True
+    else:
+        if word[low].upper() != word[high].upper():
+            return False
+        return is_palindrome_recursive(word, low + 1, high - 1)
