807be2cfa8e5402206641b0d9208dd19a3e011b2
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-10 15:06:09 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count = 0
+    t = target_time
+    for p in permanence_period:
+        try:
+            if t >= p[0] and t <= p[1]:
+                count += 1
+        except TypeError:
+            return None
+    return count
