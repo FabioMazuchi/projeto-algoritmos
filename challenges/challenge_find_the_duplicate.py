936844dee3f98276aef19a9a16d3f2a73564c649
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-10 19:37:57 -0300

    requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1] and nums[i] > 0:
+            return nums[i]
+    return False
